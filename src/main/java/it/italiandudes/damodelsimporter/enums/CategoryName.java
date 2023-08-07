bb11c5afc11f9573130d1324e13a2401c47b1057
Hacker6329 <Hacker6329@gmail.com>
2023-08-07 05:13:47 +0200

    Added Place Objects

--- src/main/java/it/italiandudes/damodelsimporter/enums/CategoryName.java
+++ src/main/java/it/italiandudes/damodelsimporter/enums/CategoryName.java
@@ -5,15 +5,30 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 
 public enum CategoryName {
-    NPCs,
-    Creatures
+    PLACE_OBJECTS("Place Objects"),
+    CHARACTERS("Characters"),
+    MONSTERS("Monsters"),
+    NPCS("NPCs"),
+
     ;
 
     // Attributes
     @NotNull public static final ArrayList<String> CATEGORY_NAMES = new ArrayList<>();
     static {
         for (CategoryName categoryName : CategoryName.values()) {
-            CATEGORY_NAMES.add(categoryName.name());
+            CATEGORY_NAMES.add(categoryName.getName());
         }
+    }
+    @NotNull private final String name;
+
+    // Constructors
+    CategoryName(@NotNull final String name) {
+        this.name = name;
+    }
+
+    // Methods
+    @NotNull
+    public String getName() {
+        return name;
     }
 }
